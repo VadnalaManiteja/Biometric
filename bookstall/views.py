55e3efa88a45fb40cb02e697711e44f8bc77d91b
unknown <sharvanikokkonda@gmail.com>
2024-12-23 11:28:23 +0530

    improvements 2

--- bookstall/views.py
+++ bookstall/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 def base(request):
     return render(request, 'base.html')
 
+from django.shortcuts import render
+from .models import Employee  # Import your Employee model
+
 def index(request):
-    return render(request,'index.html')
+    total_employees = Employee.objects.count()  # Count total employees
+    return render(request, 'index.html', {'total_employees': total_employees})
